0a317f6d1ee438a4febce15c20593de2cd35da1b
Milancitooo <164687361+Milancitooo@users.noreply.github.com>
2024-03-30 00:01:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('llevo días viendo videos de como animar para poder hacer cosas lindas como estas flores simplonas. A decir verdad estoy aprendiendo varias cosas solo para que en algún momento te las pueda dar a ti, te amo mucho, creo que por eso estoy empeñado a hacer cosas de este estilo para ti')
+
+})
